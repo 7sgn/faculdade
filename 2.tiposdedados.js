49e40b5d5840c348c02231c44ee025006cf054ae
7sgn <sette562@gmail.com>
2024-06-13 12:18:00 -0300

    primeiro commit 13/06/24

--- 2.tiposdedados.js
+++ 2.tiposdedados.js
@@ -5,10 +5,10 @@
 */ 
 
 const texto1 =  "textos com aspas duplas"
-const texto2 = 'textos com aspas simples'
-const texto3 = `texto
-                com 
-                crase`  
+const texto2 =  'textos com aspas simples'
+const texto3 =  `texto
+                 com 
+                 crase`  
 
 
 console.log(texto1)
