bed00865253ad3bf0a6f500451ee6cdd470a53a5
7sgn <sette562@gmail.com>
2024-06-14 11:31:13 -0300

    segundo commit 14/06/24

--- Css-Curso/info.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set PYTHON_PATH=C:\Users\marlon.sette\OneDrive - Sistema FIBRA\faculdade\faculdade\Css-Curso
-
-%PYTHON_PATH% "script.py"
-
-pause
--- Css-Curso/script.py
+++ Css-Curso/script.py
@@ -3,36 +3,41 @@ import platform
 import psutil
 import os
 
-txt = 'info.txt'
+
 
 def info():
     versaoso = platform.platform()
-    init = wmi.WMI()
+    inicia = wmi.WMI()
     nó = platform.node()
     usuario = os.getlogin()
     disco = psutil.disk_usage('/')
     total = disco.total / (1024 ** 3)
     mem = psutil.virtual_memory()
+    loguser = psutil.users()
+    logs = [(usuario.name) for usuario in loguser]
+    txt = f'{nó}.txt'
+
+    with open(txt, 'w') as f:
+        for processor in inicia.Win32_Processor():
+            f.write(f'Nome do processador: {processor.Name}\n')
+            break
 
 
-    for processor in init.Win32_Processor():
-        print('Nome do processador: ', processor.Name)
-        break
-
-
-    print('Nome: ', nó)
-    print(f'Usuário Logado: {usuario}')
-    print(F'Versão do SO: {versaoso}')
-    print(f'Disco: {total:.2f}GB')
-    print(f'Uso do disco: ', disco.percent, '%')
-    print(f'Memória total: {mem.total / (1024 ** 3):.2f}GB')
-    print(f'Memória em uso: {mem.used / (1024 ** 3):.2f}GB')
+        f.write(f'Nome: {nó}\n')
+        f.write(f'Usuário logado: {usuario}\n')
+        f.write(f'Logs de user: {logs}\n')
+        f.write(F'Versão do SO: {versaoso}\n')
+        f.write(f'Disco: {total:.2f}GB\n')
+        f.write(f'Uso do disco: {disco.percent}%\n')
+        f.write(f'Memória total: {mem.total / (1024 ** 3):.2f}GB\n')
+        f.write(f'Memória em uso: {mem.used / (1024 ** 3):.2f}GB\n')
 
 info()
 
-    # with open(txt, 'w') as arquivo:
-    #    arquivo.write(f'{infor}')   
-    # print(f'As informações foram salvas em "{txt}"')
+
+    
+
+
 
 
 
