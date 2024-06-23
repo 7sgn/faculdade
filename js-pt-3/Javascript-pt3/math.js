c77ede016e3793ca20ee31380b1c3b1bcbdd2d42
7sgn <sette562@gmail.com>
2024-06-23 15:04:33 -0300

    primeiro commit 23/06/24

---
+++ js-pt-3/Javascript-pt3/math.js
@@ -0,0 +1,14 @@
+/*
+Math -> Matemática
+- pow(2,2) potencia / 2² = 4
+- sqrt(25,2) / raiz / √25
+- PI
+- ceil / teto -> arredondar pra cima
+- floor / chão -> arredondar pra baixo
+
+- random() / número aleatório entre 0 e 1
+*/
+
+const result = Math.pow(2,3)
+
+console.log(result)
