3fdb5cb4087afabee74917ff33b9ab89e95f2dd7
7sgn <sette562@gmail.com>
2024-06-23 18:37:43 -0300

    Ultimo commit 26/06/24

---
+++ js-pt-3/Javascript-pt3/operadores-de-comparação.js
@@ -0,0 +1,19 @@
+/*
+    Operadores de comparação
+
+    == -> Igual // Compara o valor, mas não o tipo
+    === -> TOTALMENTE Igual // Compara o VALOR e o TIPO
+
+    != -> Diferente // Ele compara o valor, mas não o tipo
+    !== -> TOTALMENTE Diferente // Compara o VALOR e o TIPO
+
+*/
+
+const firstNumber = 30
+const seccondNummber = 40
+
+if(firstNumber == seccondNummber){
+    console.log('São iguais')
+}else {
+    console.log('Não são iguais')
+}
