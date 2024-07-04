d3b28a2011f83a500b34e2b452efd4087228581d
Marlon Sette <sette562@gmail.com>
2024-07-04 11:57:52 -0300

    Terceiro commit 04/07/24 11:57

---
+++ js-pt-4/desafio functions/return.js
@@ -0,0 +1,28 @@
+/*
+    Calcula Desconto:
+    Todos os produtos acima de R$ 30,00 tem desconto de 10%
+
+*/
+
+const cart = [10, 244, 99, 2, 20, 33, 250]
+let finalValue = 0
+
+function calculateDiscount(price, discount) {
+    const result = (price * discount) / 100
+    return result
+}
+
+cart.forEach(value => { 
+    
+    if (value > 30) {
+        const discount = calculateDiscount(value, 10)
+        finalValue = finalValue + ( value - discount )
+    } else {
+        finalValue += value
+    }
+                            
+             // aqui cerificamos o valor de cada item do array e somamos ele.
+});
+
+console.log(finalValue)
+
