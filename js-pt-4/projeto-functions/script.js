26fa27315d2f676819830abe55c5f596b1b54cf1
Marlon Sette <sette562@gmail.com>
2024-07-09 10:20:27 -0300

    Terceiro commit 09/07/24 10:20

--- js-pt-4/projeto-functions/script.js
+++ js-pt-4/projeto-functions/script.js
@@ -1,5 +1,4 @@
 const result = document.querySelector('.result')
-<<<<<<< HEAD
 const resultChoice = document.querySelector('.resultChoice')
 const humanPoint = document.querySelector('#cont1')
 const machinePoint = document.querySelector('#cont2')
@@ -8,17 +7,7 @@ let humanPointCont = 0
 let machinePointCont = 0
 
 const playHuman = (humanChoice) => {
-    
-=======
-const bothChoice = document.querySelector('.choices')
-const myChoice = document.querySelector('#cont1')
-const machineChoice = document.querySelector('#cont2')
 
-let myChoiceNumber = 0
-let machineChoiceNumber = 0
-
-const playHuman = (humanChoice) => {
->>>>>>> 643f12b189557b450ca60d86e23fce964577ede8
     playTheGame(humanChoice, playMachine())
 }
 
@@ -27,7 +16,7 @@ const playMachine = () => {
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
-<<<<<<< HEAD
+
 
 }
 
@@ -37,7 +26,7 @@ const playTheGame = (human, machine) => {
     if (human === machine) {
         result.innerHTML = 'Deu empate'
         resultChoice.innerHTML = `Você escolheu: ${human} e a Alexa escolheu: ${machine}`
-        
+
     } else if ((human === 'Papel' && machine === 'Pedra') ||
         (human === 'Pedra' && machine === 'Tesoura') ||
         (human === 'Tesoura' && machine === 'Papel')) {
@@ -53,28 +42,5 @@ const playTheGame = (human, machine) => {
         machinePointCont++
         machinePoint.innerHTML = machinePointCont
     }
-
-=======
-}   
-
-const playTheGame = (human, machine) => {
-    
-    
-     if(human === machine) {
-        result.innerHTML = 'Deu empate!'
-        bothChoice.innerHTML = `Você escolheu: ${human} e a alexa escolheu: ${machine}`
-    }else if (human === 'Papel' && machine === 'Pedra' || human === 'Pedra' && machine === 'Tesoura' || human === 'Tesoura' && machine === 'Papel') {
-        result.innerHTML = 'Você ganhou!'
-        bothChoice.innerHTML = `Você escolheu: ${human} e a alexa escolheu: ${machine}`
-        myChoiceNumber++
-        myChoice.innerHTML = myChoiceNumber
-
-    } else {
-        result.innerHTML = 'Você perdeu para a Alexa!'
-        bothChoice.innerHTML = `Você escolheu: ${human} e a alexa escolheu: ${machine}`
-        machineChoiceNumber++
-        machineChoice.innerHTML = machineChoiceNumber
-    }
-    
->>>>>>> 643f12b189557b450ca60d86e23fce964577ede8
 }
+
