6ee5af236bf4f7fa45d220343178f06d10075eb3
Marlon Sette <sette562@gmail.com>
2024-07-12 10:42:47 -0300

    Primeiro commit manutenção 12/07/24 10:42

--- js-pt-4/projeto-functions/script.js
+++ js-pt-4/projeto-functions/script.js
@@ -6,13 +6,27 @@ const machinePoint = document.querySelector('#cont2')
 let humanPointCont = 0
 let machinePointCont = 0
 
+/*
+    humanPointCont  -> Camel case
+    GAME_OPTIONS -> Snake case  // no caso de ENUMS se usa o Snake case
+
+
+*/
+
+const GAME_OPTIONS = {
+    ROCK: 'Pedra',
+    PAPER: 'Papel',
+    SCISSORS: 'Tesoura'
+}
+
+
 const playHuman = (humanChoice) => {
 
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['Pedra', 'Papel', 'Tesoura']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -27,9 +41,9 @@ const playTheGame = (human, machine) => {
         result.innerHTML = 'Deu empate'
         resultChoice.innerHTML = `Você escolheu: ${human} e a Alexa escolheu: ${machine}`
 
-    } else if ((human === 'Papel' && machine === 'Pedra') ||
-        (human === 'Pedra' && machine === 'Tesoura') ||
-        (human === 'Tesoura' && machine === 'Papel')) {
+    } else if ((human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS') ||
+        (human ===GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)) {
 
         result.innerHTML = 'Você ganhou, parabéns!'
         resultChoice.innerHTML = `Você escolheu: ${human} e a Alexa escolheu: ${machine}`
