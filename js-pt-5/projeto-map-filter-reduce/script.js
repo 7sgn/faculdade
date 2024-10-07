63c6f78a00738183c0833305398a0ca9503c6020
7sgn <sette562@gmail.com>
2024-10-07 20:53:20 -0300

    Commit 07/10/24 20:53

--- js-pt-5/projeto-map-filter-reduce/script.js
+++ js-pt-5/projeto-map-filter-reduce/script.js
@@ -3,6 +3,16 @@
 const list = document.querySelector('ul')
 const buttonFor = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
+const sumAll = document.querySelector('.sum-all')
+const filterAll = document.querySelector('.filter-all')
+
+
+function formatValue(value){
+    const newValue = value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+
+    return newValue
+}
+
 
 
 function showAll(productsArray){
@@ -13,7 +23,7 @@ function showAll(productsArray){
         <li>
             <img src=${product.src}>
             <p>${product.name}</p>
-            <p class="item-price">R$ ${product.price}</p>
+            <p class="item-price"> ${formatValue(product.price)}</p>
         </li>
     `
         
@@ -21,15 +31,44 @@ function showAll(productsArray){
     list.innerHTML = myLi
 }
 
+ 
+
+
+
+
+
+
 function mapAllItens(){
     const newPrices = menuOptions.map( (product) => ({
         ...product,
-        price: product.price * 0.9,
+        price: formatValue(product.price * 0.9),
         
     }))
     
     showAll(newPrices) 
 }
 
+function sumAllItens(){
+    const totalValue = menuOptions.reduce( (acc, curr) => acc + curr.price, 0)
+    
+    list.innerHTML = `
+    <li>
+        <p>O Valor total dos ittens é  ${formatValue(totalValue)}</p>
+    </li>
+    `
+}
+
+function filterAllItens(){
+    const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+    showAll(filterJustVegan)  
+
+}
+
+
+
+
 buttonFor.addEventListener('click', () => showAll(menuOptions))
 buttonMapAll.addEventListener('click', mapAllItens)
+sumAll.addEventListener('click', sumAllItens)
+filterAll.addEventListener('click', filterAllItens)
