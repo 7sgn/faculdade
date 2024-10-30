0cdf5828fd499f0f7432e9083fc8f8b4872a6467
7sgn <sette562@gmail.com>
2024-10-30 15:01:32 -0300

    Commit 30/10/2024

--- Conceitos-de-node/server.js
+++ Conceitos-de-node/server.js
@@ -5,26 +5,36 @@
 // NOVO
 
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
+
+
 
 const prisma = new PrismaClient()
 const app = express()
+const PORT = 3000
+
 
 app.use(express.json())
+app.use(cors())
 
 
 
 
 app.get('/usuarios', async (req, res) => {
-  
+  try{
     const users = await prisma.user.findMany()
 
     res.status(200).json(users)
- 
+} 
+  catch(error){
+    res.status(400).json({message: "Erro ao consultar usuário"})
+}
  
 })
 
 app.post('/usuarios', async (req, res) => {
+  try{
     const user = await prisma.user.create({
         data: {
             email: req.body.email,
@@ -34,10 +44,15 @@ app.post('/usuarios', async (req, res) => {
     })
 
     res.status(201).json({ message: "Usuário criado com sucesso"})
+} 
+  catch(error){
+    res.status(400).json({message: "Erro ao criar usuário"})
+}
 })
 
 app.put('/usuarios/:id', async (req, res) => {
-    const user = await prisma.user.update({
+   try{
+    const User = await prisma.user.update({
         where: {
             id: req.params.id
 
@@ -49,16 +64,25 @@ app.put('/usuarios/:id', async (req, res) => {
         }
     })
 
-    res.status(201).json({ message: "Usuário criado com sucesso"})
+    res.status(201).json({ message: "Usuário editado com sucesso"})
+} catch(error){
+    res.status(400).json({message: "Erro ao editar usuário"})
+}
 })
 
 app.delete('/usuarios/:id', async (req, res) => {
+   try{ 
     await prisma.user.delete({
         where: {
             id: req.params.id
         }
     })
     res.status(200).json({message: 'Usuario deletado com sucesso'})
+} catch(error){
+    res.status(400).json({message: "Erro ao dleetar usuário"})
+}
 })
 
-app.listen(3000)
+app.listen(PORT, () =>{
+    console.log(`O servidor está rodando na porta ${PORT}`)
+})
